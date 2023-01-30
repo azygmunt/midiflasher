f312664fd29fe527bc295c37916635e18613dc87
Aaron Zygmunt <aaronzygmunt@gmail.com>
2023-01-29 18:26:57 -0600

    metronome button modes added

--- code.py
+++ code.py
@@ -23,40 +23,43 @@ print("starting midiflasher")
 i2c = busio.I2C(sda=board.GP0, scl=board.GP1)
 lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
 
-ledCount = 60
+led_count = 60
 midi = adafruit_midi.MIDI(midi_in=usb_midi.ports[0], midi_out=usb_midi.ports[1])
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
-pixels = neopixel.NeoPixel(board.A1, ledCount, auto_write=True, brightness=1)
+pixels = neopixel.NeoPixel(board.A1, led_count, auto_write=True, brightness=1)
 encoder = rotaryio.IncrementalEncoder(board.GP10, board.GP11)
 btn = digitalio.DigitalInOut(board.GP9)
 btn.direction = digitalio.Direction.INPUT
 btn.pull = digitalio.Pull.UP
 button_previous = btn.value
 
-noteNames = ("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
-
+note_names = ("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+metronome_knob_modes = ("color", "brightness", "position")
+metronome_knob_index = 0
+metronome_knob_mode = metronome_knob_modes[metronome_knob_index]
 clock_count = 0
 beat_count = 0
 # encoder_previous_position = None
-clock_offset = int((ledCount / 2) - 12)  # 24 steps offset from center
+clock_offset = int((led_count / 2) - 12)  # 24 steps offset from center
 clock_current_pixel = 0
 clock_previous_pixel = 0
 clock_flag = False
 metronome_palette = palettes.palette_6
 metronome_palette_position = 0
-metronome_palette_previous_position = 0
+metronome_brightness = 128
+
 lcd.clear()
-lcd.print("midi flasher")
+# lcd.print("midi flasher")
 
 
 def note_position(note):
     # center around middle C (note 60)
-    offset = int((ledCount / 2) - 60)
+    offset = int((led_count / 2) - 60)
     index = note + offset
 
     # truncate ends
-    last_index = ledCount - 1
+    last_index = led_count - 1
     if index < 0:
         index = 0
     elif index > last_index:
@@ -106,41 +109,82 @@ def note_color_notes(note, velocity, channel):
     return gamma_adjusted((r, g, b))
 
 
-encoder_current_position = encoder.position
-encoder_previous_position = encoder_current_position
+knob_current_position = encoder.position
+knob_previous_position = knob_current_position
+
+lcd.print("Color: " + str(metronome_palette_position))
 
 while True:
-    msg = midi.receive()
-    encoder_current_position = encoder.position
-    if encoder_current_position != encoder_previous_position:
-        lcd.clear()
-        lcd.print(str(encoder_current_position))
-
-        # set the metronome color
-        if encoder_current_position > encoder_previous_position:
-            metronome_palette_position += 1
-            if metronome_palette_position >= len(metronome_palette):
-                metronome_palette_position = 0
-        elif encoder_current_position < encoder_previous_position:
-            metronome_palette_position -= 1
-            if metronome_palette_position < 0:
-                metronome_palette_position = len(metronome_palette) - 1
-        print(metronome_palette_position)
-
-        encoder_previous_position = encoder_current_position
-
+    # read the button and set the mode
     button_current = btn.value
     if button_current != button_previous:
         if not button_current:
-            lcd.clear()
-            lcd.print("BTN is down")
             print("BTN is down")
-        else:
+            metronome_knob_index += 1
+            if metronome_knob_index >= len(metronome_knob_modes):
+                metronome_knob_index = 0
+            metronome_knob_mode = metronome_knob_modes[metronome_knob_index]
             lcd.clear()
-            lcd.print("BTN is up")
+            # lcd.print(metronome_knob_mode)
+            if metronome_knob_mode == "color":
+                lcd.print("Color: " + str(metronome_palette_position))
+            elif metronome_knob_mode == "brightness":
+                lcd.print("Brightness: " + str(metronome_brightness))
+            elif metronome_knob_mode == "position":
+                lcd.print("Position: " + str(clock_offset))
+
+        else:
+            # lcd.clear()
+            # lcd.print("BTN is up")
             print("BTN is up")
 
     button_previous = button_current
+
+    # read the knob and change a value according to the mode
+    knob_current_position = encoder.position
+    if knob_current_position != knob_previous_position:
+        # lcd.home()
+        # lcd.clear()
+        # set the metronome color
+        if metronome_knob_mode == "color":
+            if knob_current_position > knob_previous_position:
+                metronome_palette_position += 1
+                if metronome_palette_position >= len(metronome_palette):
+                    metronome_palette_position = 0
+            elif knob_current_position < knob_previous_position:
+                metronome_palette_position -= 1
+                if metronome_palette_position < 0:
+                    metronome_palette_position = len(metronome_palette) - 1
+            lcd.set_cursor_pos(0, 7)
+            lcd.print(str(metronome_palette_position))
+            # print(metronome_palette_position)
+        elif metronome_knob_mode == "brightness":
+            if knob_current_position > knob_previous_position:
+                metronome_brightness += 4
+                if metronome_brightness >= 255:
+                    metronome_brightness = 255
+            elif knob_current_position < knob_previous_position:
+                metronome_brightness -= 4
+                if metronome_brightness < 0:
+                    metronome_brightness = 0
+            lcd.set_cursor_pos(0, 12)
+            lcd.print(str(metronome_brightness) + "  ")
+            # print(metronome_brightness)
+        elif metronome_knob_mode == "position":
+            if knob_current_position > knob_previous_position:
+                clock_offset -= 1
+                if clock_offset < 0:
+                    clock_offset = 0
+            elif knob_current_position < knob_previous_position:
+                clock_offset += 1
+                if clock_offset + 24 >= led_count:
+                    clock_offset = led_count - 24
+            lcd.set_cursor_pos(0, 10)
+            lcd.print(str(clock_offset) + " ")
+
+        knob_previous_position = knob_current_position
+
+    msg = midi.receive()
 
     if msg is not None:
         if isinstance(msg, NoteOn):
@@ -193,8 +237,9 @@ while True:
                     else:
                         clock_current_pixel = 24 - clock_count
                     clock_current_pixel += clock_offset
+                    brightness = metronome_brightness / 255
                     pixels[clock_current_pixel] = gamma_adjusted(
-                        tuple([int(b / 4) for b in metronome_palette[metronome_palette_position]])
+                        tuple([int(c * brightness) for c in metronome_palette[metronome_palette_position]])
                     )
                 clock_previous_pixel = clock_current_pixel
 
